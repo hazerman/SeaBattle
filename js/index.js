428bb89a6e3a1ce2c55de25d7f3cec42fbd242ac
hazerman <49827421+hazerman@users.noreply.github.com>
2020-03-05 15:44:27 +0500

    Добавляет полный функционал только без рандомных кораблей

--- js/index.js
+++ js/index.js
@@ -5,9 +5,11 @@ const shot = document.querySelector('#shot');
 const hit = document.querySelector('#hit');
 const dead = document.querySelector('#dead');
 const enemy = document.querySelector('#enemy');
+const reloadButton = document.querySelector('#again');
+const header = document.querySelector('.header');
 
 const game = {
-  record: 0,
+  record: localStorage.getItem('seaBattleRecord') || 0,
   shot: 0,
   hit: 0,
   dead: 0,
@@ -24,8 +26,33 @@ const game = {
 };
 
 const field = {
+  ships: [
+    {
+      location: ['11', '12', '13', '14'],
+      hit: ['', '', '', '']
+    },
+    {
+      location: ['56', '66', '76'],
+      hit: ['', '', '']
+    },
+    {
+      location: ['31', '32'],
+      hit: ['', '']
+    },
+    {
+      location: ['88'],
+      hit: ['']
+    }
+  ],
+  shipCount: 4,
   showMiss(data) {
     this.changeClass(data, 'miss');
+  },
+  showHit(data) {
+    this.changeClass(data, 'hit');
+  },
+  showDead(data) {
+    this.changeClass(data, 'dead');
   },
   changeClass(data, className) {
     data.className = className;
@@ -34,14 +61,44 @@ const field = {
 
 const fire = (evt) => {
   const target = evt.target.closest('td');
-  if (target && target.className !== 'miss') {
+  if (target && !target.classList.length) {
     field.showMiss(target);
     game.updateData = 'shot';
+    field.ships.forEach(ship => {
+      const index = ship.location.indexOf(target.id);
+      if (index >= 0) {
+        field.showHit(target);
+        game.updateData = 'hit';
+        ship.hit[index] = 'x';
+        const isDead = ship.hit.every(item => item === 'x');
+        if (isDead) {
+          game.updateData = 'dead';
+          ship.location.forEach(location => {
+            field.showDead(document.getElementById(location));
+          });
+          --field.shipCount;
+          if (field.shipCount < 1) {
+            header.textContent = 'Игра Окончена!';
+            header.style.color = 'red';
+            enemy.removeEventListener('click', fire);
+            if (game.shot < game.record || game.record === 0) {
+              localStorage.setItem('seaBattleRecord', game.shot);
+              game.record = game.shot;
+              game.render();
+            }
+          }
+        }
+      }
+    });
   }
 };
 
 const init = () => {
   enemy.addEventListener('click', fire);
+  game.render();
+  reloadButton.addEventListener('click', () => {
+    location.reload();
+  });
 };
 
 init();
